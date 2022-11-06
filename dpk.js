7c44e8c5c02d5adb03f760ea45bc79de481f12b6
Chaoneng Tan <chaonengtan@gmail.com>
2022-11-05 18:21:37 -0700

    +ternary

--- dpk.js
+++ dpk.js
@@ -1,23 +1,19 @@
 const crypto = require("crypto");
 
 exports.deterministicPartitionKey = (event) => {
+  // consts
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
+  // helper function
   const createNewHash = (updateValue) => {
     // creates a sha3-512 crypto hash, runs .update with parameter value, and digests in hex
     return crypto.createHash("sha3-512").update(updateValue).digest("hex");
   };
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = createNewHash(data);
-    }
-  }
+  // if event has partition key, use partion key else, createNewHash using the stringified event
+  if (event) { candidate = event.partitionKey ? event.partitionKey : createNewHash(JSON.stringify(event)) }
 
   if (candidate) {
     if (typeof candidate !== "string") {
